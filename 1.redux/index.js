8ac2d59fe7f40c42fa08fea83483c3ea6e6f3299
colo1211 <colo1211@naver.com>
2021-12-27 17:36:30 +0900

    1.redux

---
+++ 1.redux/index.js
@@ -0,0 +1,37 @@
+const { createStore } = require('redux'); 
+
+
+const reducer = (prevState, action) => {
+    switch(action.type){
+        case 'CHANGE_COMP_A':
+            return{
+                compA : action.data, 
+                compB : 12, 
+                compC : null,
+            }
+    }
+}; 
+
+const initialState = {
+    compA : 'a', 
+    compB : 12, 
+    compC : null,
+};
+
+const store = createStore(reducer, initialState); 
+
+// 변경 전
+console.log(store.getState()); 
+
+// action
+const changeCompA = (data)=>{
+    return {
+        type : 'CHANGE_COMP_A',
+        data,
+    }
+}
+
+store.dispatch(changeCompA('b'));
+
+// 변경 후
+console.log(store.getState()); 
