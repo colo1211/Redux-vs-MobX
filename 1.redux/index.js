515244c409e74624560c43e2b781b8ad1853cd35
colo1211 <colo1211@naver.com>
2021-12-27 19:26:19 +0900

    redux 예제

--- 1.redux/index.js
+++ 1.redux/index.js
@@ -1,14 +1,26 @@
 const { createStore } = require('redux'); 
 
 
+// 새로운 state 를 만들어 주는 역할
 const reducer = (prevState, action) => {
     switch(action.type){
         case 'CHANGE_COMP_A':
             return{
+                ...prevState,
                 compA : action.data, 
-                compB : 12, 
-                compC : null,
             }
+        case 'CHANGE_COMP_B':
+            return{
+                ...prevState,
+                compB : action.data, 
+            }
+        case 'CHANGE_COMP_C':
+            return{
+                ...prevState,
+                compC : action.data, 
+            }
+        default : 
+            return prevState;  
     }
 }; 
 
@@ -20,10 +32,16 @@ const initialState = {
 
 const store = createStore(reducer, initialState); 
 
-// 변경 전
-console.log(store.getState()); 
+// subscribe 는 react-redux 안에 들어있음, 에러 디버깅 할 때 사용
+store.subscribe(()=>{
+    console.log('changed'); 
+})
 
-// action
+
+// 변경 전
+console.log(store.getState()); // { compA: 'a', compB: 12, compC: null }
+
+// action 생성 함수
 const changeCompA = (data)=>{
     return {
         type : 'CHANGE_COMP_A',
@@ -31,7 +49,10 @@ const changeCompA = (data)=>{
     }
 }
 
+
+// dispatch
 store.dispatch(changeCompA('b'));
 
 // 변경 후
-console.log(store.getState()); 
+console.log(store.getState()); // { compA: 'b', compB: 12, compC: null }
+
